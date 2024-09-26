c55fcf3d88644ab8eadf6a68c231a8bdfaca2c30
Anna Kapitanova <totoshechka19@gmail.com>
2024-09-26 18:10:06 +0300

    сделала not-found-page.tsx

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,6 +4,7 @@ import CatalogPage from '../pages/catalog-page';
 import ProductPage from '../pages/product-page';
 import BasketPage from '../pages/basket-page';
 import {AppRoute} from '../conts';
+import NotFoundPage from '../pages/not-found-page';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path={AppRoute.Catalog} element={<CatalogPage />} />
           <Route path={AppRoute.Product} element={<ProductPage />} />
           <Route path={AppRoute.Basket} element={<BasketPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </HelmetProvider>
---
+++ src/pages/not-found-page.tsx
@@ -0,0 +1,26 @@
+import { Helmet } from 'react-helmet-async';
+import Layout from '../components/layout';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NotFoundPage() {
+  return (
+    <>
+      <Helmet>
+        <title>Страница не найдена - Фотошоп</title>
+      </Helmet>
+
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100vh' }}>
+        <Layout>
+          <div className="container" style={{ textAlign: 'center' }}>
+            <h1 className="title title--h2" style={{ marginBottom: '30px' }}>Страница не найдена</h1>
+            <Link className="btn btn--purple" to="/">Вернуться на главную страницу</Link>
+          </div>
+        </Layout>
+      </div>
+
+    </>
+  );
+}
+
+export default NotFoundPage;
