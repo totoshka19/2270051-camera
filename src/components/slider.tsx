9ba10b75038985c7b87462c2f8b8111fda818648
Anna Kapitanova <totoshechka19@gmail.com>
2024-10-19 14:42:15 +0300

    добавила стили к слайдеру

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -31,6 +31,25 @@ function Slider() {
           </div>
         </SwiperSlide>
       ))}
+      <style>
+        {`
+          .swiper-pagination {
+            text-align: right;
+            padding-right: 40px;
+          }
+          .swiper-pagination-bullet {
+            width: 16px;
+            height: 16px;
+            background-color: #f4f4fc;
+            border-radius: 50%;
+            margin: 0 7px;
+            opacity: 1;
+          }
+          .swiper-pagination-bullet-active {
+            background-color: #7575e2;
+          }
+        `}
+      </style>
     </Swiper>
   );
 }
