856297755da0a229c0aa1c3b0cdf1e19fe6f5801
Anna Kapitanova <totoshechka19@gmail.com>
2024-12-29 14:33:24 +0300

    убрала ошибку линтера в localStorageMiddleware

--- src/store/local-storage-middleware.ts
+++ src/store/local-storage-middleware.ts
@@ -1,15 +1,12 @@
 import { Middleware } from '@reduxjs/toolkit';
 import { RootState } from './root-reducer';
 
-export const localStorageMiddleware: Middleware = (store) => (next) => (action) => {
-  const result = next(action);
+export const localStorageMiddleware: Middleware<object, RootState> = (store) => (next) => (action) => {
+  const result: unknown = next(action);
+  const state = store.getState();
 
-  const state = store.getState() as RootState;
-
-  if (state.basket && typeof state.basket === 'object' && 'items' in state.basket) {
-    if (Array.isArray(state.basket.items)) {
-      localStorage.setItem('basket', JSON.stringify(state.basket));
-    }
+  if (state.basket && Array.isArray(state.basket.items)) {
+    localStorage.setItem('basket', JSON.stringify(state.basket));
   }
 
   return result;
